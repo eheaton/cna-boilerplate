635ea18c96ddd0a6c7c1316e639afe458d565594
Eric Heaton <eric@fgsake.org>
2023-06-22 12:22:33 -0400

    fix(template): correct default mocks naming

--- .fs-templates/react-ts-component/__ENV_FS_NAME_PASCALCASE.mocks.ts
+++ .fs-templates/react-ts-component/__ENV_FS_NAME_PASCALCASE.mocks.ts
@@ -1,7 +1,9 @@
 import type { I__ENV_FS_NAME_PASCALCASE } from './__ENV_FS_NAME_PASCALCASE';
 
-const base: I__ENV_FS_NAME_PASCALCASE = {};
+export const base: I__ENV_FS_NAME_PASCALCASE = {};
 
-export const mockBaseTemplateProps = {
+const mocks = {
   base,
 };
+
+export default mocks;
